f850a0ba29985deb46e93a0d6cab57f4e47335a8
EC2 Default User <ec2-user@ip-172-31-57-108.ec2.internal>
2021-09-09 18:35:42 +0000

    Initial Commit

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
+
+  has_many :books, dependent: :destroy
+  attachment :profile_image
+
+   def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+   end
+end
